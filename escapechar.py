1ad171341f377ec6d4c02b5e64a287b6d5fed961
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-05-18 11:37:27 -0400

    Hello World - Python Lesson

---
+++ escapechar.py
@@ -0,0 +1,13 @@
+splitStr = "This string has been\nsplit over\nseveral\nlines"
+
+print(splitStr)
+
+tabbedStr = "1\t2\t3\t4\t5"
+print(tabbedStr)
+
+print('Jeffrey\'s dog is "Pepsi"')
+print("He said \"nah\"")
+print('''asdf's''')
+
+print('C:\\users\\tim\\notes')
+print(r"C:\users\tim\notes")
---
+++ strings.py
@@ -0,0 +1,11 @@
+print('Today is a good day to learn Python')
+print("Python is fun")
+print("Python strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+
+greeting = "Hello"
+name = input("Please enter your name ")
+
+# notes are written with #
+print(greeting + " " + name)
