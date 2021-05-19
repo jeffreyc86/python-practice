b5251699638ab5efc2d633176c5fe5cfa8ab022a
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-05-19 14:52:02 -0400

    python practice 5/19

---
+++ operators.py
@@ -0,0 +1,12 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a/b) # single / returns float
+print(a // b) #integer division, rounded down towards minus infinity
+print(a % b) #modulo
+
+for i in range(1, 7):
+    print(i+1)
---
+++ strings2.py
@@ -0,0 +1,32 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3])
+
+for char in parrot:
+    print(char)
+
+print()
+
+dog = "Pepsi"
+for i in range(0, len(dog)):
+    print(dog[i])
+
+# negative indexes
+
+print(dog[-2])  # returns s
+
+# slicing
+print(parrot[0:7]) # Norwegi
+
+print(dog[1:]) # epsi
+print(dog[:4]) #Peps
+
+print(parrot[-7:13])  # an Blu
+print(parrot[10:1])     # because ending index is before starting index, an empty str is returned
+
+# slicing with step
+
+dog = "Pepsi is good"
+print(dog[2:-2:2]) # pii o
